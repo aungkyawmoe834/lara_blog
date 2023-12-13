c4ccf27a4a5d64fdcb84e1114d1ee70c59ff5624
aungkyawmoe834 <cesaraung.2000@gmail.com>
2023-12-13 21:57:55 +0630

    week4 start

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,66 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
+
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
 
 class HomeController extends Controller
 {
-    //
-    public function index(){
-        $data =Post::all();
-        // dd($data);
-    return view('home',compact('data'));
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/',[HomeController::class,'index']);
+
+Route::resource('posts',HomeController::class);
